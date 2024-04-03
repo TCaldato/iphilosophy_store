9a3d8a4ced718eac053b1f971f464c6ba918f8bc
TCaldato <tcaldato@gmail.com>
2024-04-03 20:49:23 +0100

    Added bag context and delivery logic

--- iphilosophy_store/settings.py
+++ iphilosophy_store/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contexts',
             ],
         },
     },
@@ -166,3 +167,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10

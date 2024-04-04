a50947f54e6acaae19848b1ac12ce6cf0984f24c
TCaldato <tcaldato@gmail.com>
2024-04-04 15:22:59 +0100

    Added templatebags folder with bag_tools and init.py file. Changed the js code on the bag.html to ensure better scoping. Added adjust_bag and remore_from_bag on views.py on Bag app

--- bag/views.py
+++ bag/views.py
@@ -1,24 +1,50 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
 
 # Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity', 1))
+        redirect_url = request.POST.get('redirect_url')
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+        bag = request.session.get('bag', {})
+        bag[item_id] = bag.get(item_id, 0) + quantity
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
-    
+        request.session['bag'] = bag
+        return redirect(redirect_url)
+
+    return redirect(reverse('view_bag'))
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity', 0))
+
+        bag = request.session.get('bag', {})
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id, None)
+
+        request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        bag.pop(item_id, None)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    return HttpResponse(status=400)

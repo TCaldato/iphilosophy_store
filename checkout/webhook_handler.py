16b82422f83e558f42c00590141d7b63a38e8fb7
TCaldato <tcaldato@gmail.com>
2024-04-16 17:11:53 +0100

    Changed webhooke_handler.py. Payment_intent.succeeded is still failing. Change.succeeded and payment_intent.created on stripe succeeded

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 import json
 import time
 import uuid
+import stripe
 
 from django.http import HttpResponse
 from django.core.mail import send_mail
@@ -58,9 +59,12 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+
+        billing_details = stripe_charge.billing_details  # updated
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)  # updated
 
         # Clean data in the shipping details
         shipping_details.address = {k: v if v != "" else None for k, v in shipping_details.address.items()}

0189ae87f24acac09f64087ba2332690f5403628
TCaldato <tcaldato@gmail.com>
2024-04-18 14:03:13 +0100

    base.css and login/reg refactor

--- iphilosophy_store/settings.py
+++ iphilosophy_store/settings.py
@@ -216,3 +216,19 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
+
+#SMTP on heroku is not deploying, version of the Django and Python
+#are not compatible. Tryed to change the requirements for SMTP but all requirements
+#need to be changed. 
+#More information in the ReadMe.
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'iphilosophy@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')

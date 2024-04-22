bd593c4e26374a612d75cc399ec6061475ebd6d2
TCaldato <tcaldato@gmail.com>
2024-04-22 22:35:41 +0100

    Adding edit comments in the blog app. Added iphilosophy.jpg in the about. Added small text in about app.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # iphilosophy_store
+
     It is being developed
+
     You can check the live website [here](https://iphilosophy-store-81d2532ac343.herokuapp.com/)
--- about/admin.py
+++ about/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import About
 from django_summernote.admin import SummernoteModelAdmin
+from .models import About, CollaborateRequest
 
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
